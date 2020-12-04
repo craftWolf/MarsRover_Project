c3a8428ff96f7d1521978bae3f86d8f994b50fe0
Marie <m.a.matzdorf@student.tudelft.nl>
2020-12-04 10:21:24 +0100

    changed names in line tracker for FINAL

--- FINAL/Line Tracker.vhd
+++ FINAL/Line Tracker.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 -- entity controller
-entity CH6_controller is
+entity line_tracker is
 	port (	clk			: in	std_logic;
 		reset			: in	std_logic;
 
@@ -21,14 +21,14 @@ entity CH6_controller is
 		motor_r_reset		: out	std_logic;
 		motor_r_direction	: out	std_logic
 	);
-end entity CH6_controller;
+end entity line_tracker;
 
 -- behavioural architecture of controller
-architecture behavioural of CH6_controller is
+architecture behavioural of line_tracker is
 
-type CH6_controller_state is (RESET_STATE, FORWARD, TURN_LEFT, SHARP_LEFT, TURN_RIGHT, SHARP_RIGHT);
+type tracker_controller_state is (RESET_STATE, FORWARD, TURN_LEFT, SHARP_LEFT, TURN_RIGHT, SHARP_RIGHT);
 
-signal state, new_state : CH6_controller_state;
+signal state, new_state : tracker_controller_state;
 
 begin
 process(clk, reset)
